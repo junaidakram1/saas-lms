9151dda7f5e5d8a8ed8eefddbb74e1e922cc4d8d
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-09-27 23:04:01 +0500

    Add Guide Cards (Lessons) in Dashboard (Homepage)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
-import { Button } from "@/components/ui/button";
+import GuideCard from "@/components/GuideCard";
 
 const Page = () => {
-  return <div>home</div>;
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <section className="home-section">
+        <GuideCard
+          id="789"
+          name="Mansion Designer"
+          topic="Industrial Design"
+          subject="Design"
+          duration={45}
+          color="#7cff6e"
+        />
+        <GuideCard
+          id="789"
+          name="Eye for Eye"
+          topic="Optometry"
+          subject="science"
+          duration={40}
+          color="#caf456"
+        />
+        <GuideCard
+          id="712"
+          name="Meteoroids Bender"
+          topic="Analytical Geometry"
+          subject="maths"
+          duration={50}
+          color="#6eb2ff"
+        />
+        <GuideCard
+          id="123"
+          name="Hypnotic Finger"
+          topic="Copy Writing"
+          subject="english"
+          duration={30}
+          color="#ff6ea6"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default Page;
