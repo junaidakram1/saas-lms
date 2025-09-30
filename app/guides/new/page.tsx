01e1503f93ce5bbdca08a3ffd94b39ee13dfdf62
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-09-30 13:03:47 +0500

    feat: add new guide form using react-hook-form

--- app/guides/new/page.tsx
+++ app/guides/new/page.tsx
@@ -1,5 +1,15 @@
+import GuideForm from "@/components/GuideForm";
+
 const NewGuide = () => {
-  return <div>New Guide</div>;
+  return (
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center py-5">
+      <article className="w-full gap-4 flex flex-col">
+        <h1 className="text-center">Guide Creator</h1>
+        <h2 className="text-center font-semibold">Build a New Guide</h2>
+        <GuideForm />
+      </article>
+    </main>
+  );
 };
 
 export default NewGuide;
