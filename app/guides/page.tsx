58e4f85559776fc64e122caeb31564c9a7eacc48
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-10-01 06:16:35 +0500

    feat: add guide library component & implement getAllGuides server action

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -1,5 +1,38 @@
-const GuideLibrary = () => {
-  return <div>Guide Library</div>;
+import { getAllGuides } from "@/lib/actions/guide.actions";
+import GuideCard from "@/components/GuideCard";
+import { getSubjectColor } from "@/lib/utils";
+
+const GuideLibrary = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const filters = searchParams;
+
+  const subject = filters.subject ? String(filters.subject) : "";
+  const topic = filters.topic ? String(filters.topic) : "";
+
+  const guides = await getAllGuides({ subject, topic, page: 1, limit: 10 });
+
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Guide Learning Hub</h1>
+        <div className="flex gap-4">
+          <h3>Filters</h3>
+        </div>
+      </section>
+      <section className="companions-grid">
+        {guides.map((guide) => (
+          <GuideCard
+            key={guide.id}
+            {...guide}
+            color={getSubjectColor(guide.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default GuideLibrary;
--- lib/actions/guide.actions.ts
+++ lib/actions/guide.actions.ts
@@ -17,3 +17,32 @@ export const createGuide = async (formData: CreateGuide) => {
 
   return data[0];
 };
+
+export const getAllGuides = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllGuides) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from("guides").select();
+
+  if (subject && topic) {
+    query = query
+      .ilike("subject", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  } else if (subject) {
+    query = query.ilike("subject", `%${subject}%`);
+  } else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const { data: guides, error } = await query;
+
+  if (error) throw new Error(error.message);
+
+  return guides;
+};
